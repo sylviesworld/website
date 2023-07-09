e31b594d6dd8ad7112aab7bb8cb6a2eb44696880
sylviesworld <sylvia@sylviesworld.com>
2023-07-09 17:49:46 -0500

    test

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        bagel: "'Bagel Fat One', cursive",
+      },
+    },
+  },
+  plugins: [],
+}
+
